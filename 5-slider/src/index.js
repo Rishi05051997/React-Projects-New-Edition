86ec60520c5861b12310f67064ca2d8fd5c10f90
rishi05051997 <vrushabhdhatral10@gmail.com>
2021-07-14 08:41:38 +0530

    slider project completed

---
+++ 5-slider/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import Alternative from './alternative'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Alternative />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
